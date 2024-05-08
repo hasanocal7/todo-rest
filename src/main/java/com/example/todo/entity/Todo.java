cd64ead67b8b17912aed117ef578beeb00e85a5b
hasan <hasanocal7@gmail.com>
2024-05-08 15:38:07 +0300

    Edit Todo Entity

--- src/main/java/com/example/todo/entity/Todo.java
+++ src/main/java/com/example/todo/entity/Todo.java
@@ -1,25 +1,22 @@
 package com.example.todo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Getter
+@Setter
 @Table(name = "todos")
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     @Column(name = "title")
     private String title;
     @Column(name = "description")
     private String description;
     @Column(name = "isActive")
-    private boolean isActive;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
+    private Boolean isActive = Boolean.FALSE;
 }
